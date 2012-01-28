57db745721fa133d4987b44e77837c4b761c9842
Leon Berenschot <leipeleon@gmail.com>
2012-01-28 13:07:35 +0100

    use combined log format

--- snip.rb
+++ snip.rb
@@ -30,6 +30,34 @@ class Snip < ActiveRecord::Base
   end
 end
 
+# Overide the logging and print the referer/useragent
+module Rack
+  class CommonLogger
+    FORMAT_COMBINED = %{%s - %s [%s] "%s %s%s %s" %d %s %0.4f "%s" "%s"\n}
+    private
+      def log(env, status, header, began_at)
+        now = Time.now
+        length = extract_content_length(header)
+
+        logger = @logger || env['rack.errors']
+        logger.write FORMAT_COMBINED % [
+          env['HTTP_X_FORWARDED_FOR'] || env["REMOTE_ADDR"] || "-",
+          env["REMOTE_USER"] || "-",
+          now.strftime("%d/%b/%Y %H:%M:%S"),
+          env["REQUEST_METHOD"],
+          env["PATH_INFO"],
+          env["QUERY_STRING"].empty? ? "" : "?"+env["QUERY_STRING"],
+          env["HTTP_VERSION"],
+          status.to_s[0..3],
+          length,
+          now - began_at,
+          env["HTTP_REFERER"],
+          env["HTTP_USER_AGENT"]
+          ]
+      end
+  end
+end
+
 error ActiveRecord::RecordNotFound do
   haml :index
 end
